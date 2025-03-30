1aa26031c5b0768e992787d580ca8dcec3614a2c
Axewowski <101266311+Axewowski@users.noreply.github.com>
2025-03-31 00:10:49 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import discord
 from discord.ext import commands
 import os
 
-intents = discord.Intents.default()
-intents.message_content = True
+intents = discord.Intents.none()
+intents.message_content = True  # tylko to
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
